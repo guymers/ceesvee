9cc6092998da133f3c8e4a8f8cf0b09885352f94
Sam Guymer <sam@guymer.me>
2023-09-28 19:49:45 +1000

    Optimize Scala 3 derivation
    Avoid making a new instance of an encoder/decoder in the inline
    derivation methods.
    
    Ensure laziness of summoned encoders/decoders is respected until a
    method is accessed.

--- modules/core/src/main/scala-3/ceesvee/CsvRecordEncoderDeriveScalaVersion.scala
+++ modules/core/src/main/scala-3/ceesvee/CsvRecordEncoderDeriveScalaVersion.scala
@@ -6,33 +6,35 @@ import scala.deriving.Mirror
 
 trait CsvRecordEncoderDeriveScalaVersion { self: CsvRecordEncoder.type =>
 
-  inline def summonAll[T <: Tuple]: List[CsvRecordEncoder[?]] = {
-    inline erasedValue[T] match {
-      case _: EmptyTuple => Nil
-      case _: (t *: ts) => summonInline[CsvRecordEncoder[t]] :: summonAll[ts]
-    }
+  inline def summonAll[T <: Tuple]: List[CsvRecordEncoder[?]] = inline erasedValue[T] match {
+    case _: EmptyTuple => Nil
+    case _: (t *: ts) => summonInline[CsvRecordEncoder[t]] :: summonAll[ts]
   }
 
   inline def derived[A](using m: Mirror.ProductOf[A]): CsvRecordEncoder[A] = {
-    lazy val instances = summonAll[m.MirroredElemTypes]
-
-    new CsvRecordEncoder[A] {
-      override val numFields = instances.foldLeft(0)(_ + _.numFields)
-      override def encode(a: A) = encodeUsingInstances(instances, numFields)(a)
-    }
+    lazy val encoders = summonAll[m.MirroredElemTypes]
+    new CsvRecordEncoderInstance(encoders)
   }
+}
 
+class CsvRecordEncoderInstance[A](encoders: => List[CsvRecordEncoder[?]]) extends CsvRecordEncoder[A] {
+  private lazy val instances = encoders
+
+  override lazy val numFields = instances.foldLeft(0)(_ + _.numFields)
   @SuppressWarnings(Array(
     "org.wartremover.warts.AsInstanceOf",
     "org.wartremover.warts.MutableDataStructures",
+    "org.wartremover.warts.Var",
   ))
-  private def encodeUsingInstances[A](instances: List[CsvRecordEncoder[?]], numFields: Int)(a: A) = {
+  override def encode(a: A) = {
     val builder = IndexedSeq.newBuilder[String]
     builder.sizeHint(numFields)
 
-    instances.zipWithIndex.foreach { case (p: CsvRecordEncoder[a], index) =>
+    var index = 0
+    instances.foreach { case (encoder: CsvRecordEncoder[a]) =>
       val v = a.asInstanceOf[Product].productElement(index).asInstanceOf[a]
-      builder.addAll(p.encode(v))
+      builder.addAll(encoder.encode(v))
+      index = index + 1
     }
 
     builder.result()
