afc5438a05b35e786fcad8bcb9576a59676bb08d
Sam Guymer <sam@guymer.me>
2023-09-28 10:56:35 +0100

    Optimize Scala 3 derivation (#6)
    Avoid making a new instance of an encoder/decoder in the inline
    derivation methods.
    
    Ensure laziness of summoned encoders/decoders is respected until a
    method is accessed.

--- modules/core/src/main/scala-3/ceesvee/CsvRecordDecoderDeriveScalaVersion.scala
+++ modules/core/src/main/scala-3/ceesvee/CsvRecordDecoderDeriveScalaVersion.scala
@@ -7,44 +7,48 @@ import scala.deriving.Mirror
 
 trait CsvRecordDecoderDeriveScalaVersion { self: CsvRecordDecoder.type =>
 
-  inline def summonAll[T <: Tuple]: List[CsvRecordDecoder[?]] = {
-    inline erasedValue[T] match {
-      case _: EmptyTuple => Nil
-      case _: (t *: ts) => summonInline[CsvRecordDecoder[t]] :: summonAll[ts]
-    }
+  inline def summonAll[T <: Tuple]: List[CsvRecordDecoder[?]] = inline erasedValue[T] match {
+    case _: EmptyTuple => Nil
+    case _: (t *: ts) => summonInline[CsvRecordDecoder[t]] :: summonAll[ts]
   }
 
   inline def derived[A](using m: Mirror.ProductOf[A]): CsvRecordDecoder[A] = {
-    lazy val instances = summonAll[m.MirroredElemTypes]
-
-    new CsvRecordDecoder[A] {
-      private val length = instances.length
-
-      override val numFields = instances.foldLeft(0)(_ + _.numFields)
-      override def decode(fields: IndexedSeq[String]) = {
-        decodeUsingInstances(instances, length)(fields).map(values => m.fromProduct(Tuple.fromArray(values)))
-      }
-    }
+    lazy val decoders = summonAll[m.MirroredElemTypes]
+    new CsvRecordDecoderInstance[A](m, decoders)
   }
+}
+
+class CsvRecordDecoderInstance[A](
+  m: Mirror.ProductOf[A],
+  decoders: => List[CsvRecordDecoder[?]],
+) extends CsvRecordDecoder[A] {
+  private lazy val instances = decoders
+  private lazy val length = instances.length
+
+  override lazy val numFields = instances.foldLeft(0)(_ + _.numFields)
 
   @SuppressWarnings(Array(
-    "org.wartremover.warts.AsInstanceOf",
     "org.wartremover.warts.MutableDataStructures",
+    "org.wartremover.warts.Var",
   ))
-  private def decodeUsingInstances[A](instances: List[CsvRecordDecoder[?]], length: Int)(fields: IndexedSeq[String]) = {
-    val errs = SortedMap.newBuilder[Int, Errors.Error]
+  override def decode(fields: IndexedSeq[String]) = {
+    val errs = SortedMap.newBuilder[Int, CsvRecordDecoder.Errors.Error]
     val values = Array.ofDim[Any](length)
 
-    val _ = instances.foldLeft((0, 0)) { case ((index, offset), p) =>
-      val num = p.numFields
-      p.decode(fields.slice(offset, offset + num)) match {
-        case Left(error) => error.errors.foreach { case (k, v) => errs.addOne((k + offset, v)) }
+    var index = 0
+    var offset = 0
+    instances.foreach { decoder =>
+      val num = decoder.numFields
+      decoder.decode(fields.slice(offset, offset + num)) match {
+        case Left(error) => error.errors.foreachEntry { case (k, v) => errs.addOne((k + offset, v)) }
         case Right(v) => values.update(index, v)
       }
-      (index + 1, offset + num)
+      index = index + 1
+      offset = offset + num
     }
 
     val errs_ = errs.result()
-    if (errs_.nonEmpty) Left(Errors(fields, errs_)) else Right(values)
+    if (errs_.nonEmpty) Left(CsvRecordDecoder.Errors(fields, errs_))
+    else Right(m.fromProduct(Tuple.fromArray(values)))
   }
 }
