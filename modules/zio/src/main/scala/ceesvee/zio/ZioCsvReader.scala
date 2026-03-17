57ef3b5af8d79bc1021f6123a1c98d93f43d22a3
Sam Guymer <sam@guymer.me>
2026-03-17 20:53:23 +1000

    Better ZIO decode with header return type
    Given that a CSV stream with a header also needs to be parsed, parse the entire stream and use the
    first item as the header to create the decoder and apply the decoder to every other item.
    
    This avoids having the stream wrapped in a scoped `ZIO` making the return type much nicer to use.

--- modules/zio/src/main/scala/ceesvee/zio/ZioCsvReader.scala
+++ modules/zio/src/main/scala/ceesvee/zio/ZioCsvReader.scala
@@ -1,9 +1,7 @@
 package ceesvee.zio
 
 import _root_.zio.Cause
-import _root_.zio.Scope
 import _root_.zio.Trace
-import _root_.zio.ZIO
 import _root_.zio.stream.ZPipeline
 import _root_.zio.stream.ZStream
 import ceesvee.CsvHeader
@@ -11,31 +9,54 @@ import ceesvee.CsvParser
 import ceesvee.CsvReader
 import ceesvee.CsvRecordDecoder
 
+import scala.util.control.NoStackTrace
+
 object ZioCsvReader {
-  import CsvParser.Error
+
+  // replace with a union instead of redefining when removing Scala 2 support
+  sealed trait Error
+  object Error {
+    final case class LineTooLong(maximum: Int)
+      extends RuntimeException(s"CSV line exceeded maximum length of ${maximum.toString}")
+      with Error
+
+    final case class MissingHeaders(missing: ::[String])
+      extends RuntimeException(s"Missing headers: ${missing.mkString(", ")}")
+      with NoStackTrace
+      with Error
+  }
 
   /**
    * Turns a stream of strings into a stream of decoded CSV records.
    *
    * CSV lines are reordered based on the given headers.
    */
+  @SuppressWarnings(Array("org.wartremover.warts.Null", "org.wartremover.warts.TryPartial", "org.wartremover.warts.Var"))
   def decodeWithHeader[R, E, T](
     stream: ZStream[R, E, String],
     header: CsvHeader[T],
     options: CsvReader.Options,
   )(implicit
     trace: Trace,
-  ): ZIO[Scope & R, Either[Either[E, Error], CsvHeader.MissingHeaders], ZStream[R, Either[E, Error], Either[CsvHeader.Errors, T]]] = {
-    for {
-      tuple <- ZioCsvParser.parseWithHeader(stream, options).mapError(Left(_))
-      (headerFields, s) = tuple
-      decoder <- header.create(headerFields) match {
-        case Left(error) => ZIO.refailCause(Cause.fail(error)).mapError(Right(_))
-        case Right(decoder) => ZIO.succeed(decoder)
+  ): ZStream[R, Either[E, Error], Either[CsvHeader.Errors, T]] = ZStream.suspend {
+    var decoder: CsvHeader.Decoder[T] = null
+
+    stream.mapError(Left(_)).via {
+      ZioCsvParser.parse(options).mapError {
+        case CsvParser.Error.LineTooLong(maximum) => Right(Error.LineTooLong(maximum))
       }
-    } yield {
-      s.map(decoder.decode(_))
-    }
+    }.map { fields =>
+      if (decoder eq null) {
+        decoder = header.create(fields).left.map {
+          case CsvHeader.MissingHeaders(missing) => Error.MissingHeaders(missing)
+        }.toTry.get
+        null
+      } else {
+        decoder.decode(fields)
+      }
+    }.filter(_ ne null).catchSomeCause {
+      case Cause.Die(e: Error, _) => ZStream.fail(Right(e))
+    } ++ (if (decoder eq null) ZStream.fail(Right(Error.MissingHeaders(header.headers))) else ZStream.empty)
   }
 
   /**
@@ -46,7 +67,7 @@ object ZioCsvReader {
   )(implicit
     D: CsvRecordDecoder[T],
     trace: Trace,
-  ): ZPipeline[Any, Error, String, Either[CsvRecordDecoder.Errors, T]] = {
+  ): ZPipeline[Any, CsvParser.Error, String, Either[CsvRecordDecoder.Errors, T]] = {
     ZioCsvParser.parse(options) >>> ZPipeline.map(D.decode(_))
   }
 }
